b7cfc3643dbfdf05a402d90964fd948594053c41
Jonathan Erasmus Davies <146955969+jonathanerasmus0@users.noreply.github.com>
2024-07-09 18:59:14 +0200

    updated forms

--- nottodo/tasks.py
+++ nottodo/tasks.py
@@ -1,11 +1,13 @@
-# nottodo/tasks.py
 from django.core.mail import send_mail
 from django.utils import timezone
 from .models import NotToDo
-
 def send_nottodo_notifications():
-    now = timezone.now() + timezone.timedelta(minutes=10)
-    nottodos = NotToDo.objects.filter(scheduled_time__lte=now, scheduled_time__gte=timezone.now())
+    now = timezone.now()
+    time_threshold = now + timezone.timedelta(minutes=10)
+    nottodos = NotToDo.objects.filter(
+        scheduled_start_time__lte=time_threshold,
+        scheduled_start_time__gte=now
+    )
     for nottodo in nottodos:
         send_mail(
             'Not To Do Reminder',
