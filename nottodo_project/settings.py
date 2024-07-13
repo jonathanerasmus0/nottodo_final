ca42f60154ed261f37331c93d345b171446de549
Jonathan Erasmus Davies <146955969+jonathanerasmus0@users.noreply.github.com>
2024-07-13 18:43:44 +0200

    Updated settings

--- nottodo_project/settings.py
+++ nottodo_project/settings.py
@@ -112,7 +112,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if not os.getenv('DJANGO_DEVELOPMENT'):
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp-mail.outlook.com'
+    EMAIL_HOST = 'smtp.example.com'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
     EMAIL_HOST_USER = 'joesaudi@hotmail.com'
