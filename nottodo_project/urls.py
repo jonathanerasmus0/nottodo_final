927773625cc14c330833f9795e49323e98bd7d83
Jonathan Erasmus Davies <146955969+jonathanerasmus0@users.noreply.github.com>
2024-07-19 09:37:24 +0200

    video

--- nottodo_project/urls.py
+++ nottodo_project/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('nottodo.urls')),
-    path('schema/$', Schema.as_view()),
+    path('schema/', Schema.as_view()),
 ]
